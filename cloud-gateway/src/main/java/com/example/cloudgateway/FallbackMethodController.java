a19e7f4cc12d8c3bea137029bc34ac28595fdbc5
Andrei Sold <sold_andrei@yahoo.com>
2022-04-16 12:49:58 +0300

    Fixed the handling of down microservices

--- cloud-gateway/src/main/java/com/example/cloudgateway/FallbackMethodController.java
+++ cloud-gateway/src/main/java/com/example/cloudgateway/FallbackMethodController.java
@@ -1,17 +1,21 @@
 package com.example.cloudgateway;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FallbackMethodController {
 
     @GetMapping("/userServiceFallback")
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
     public String userServiceFallbackMethod() {
         return "User Service is taking longer than expected. Please try again later.";
     }
 
     @GetMapping("/departmentServiceFallback")
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
     public String departmentServiceFallbackMethod() {
         return "Department Service is taking longer than expected. Please try again later.";
     }
--- users-service/src/main/java/com/example/userdemo/services/UserService.java
+++ users-service/src/main/java/com/example/userdemo/services/UserService.java
@@ -5,9 +5,15 @@ import com.example.userdemo.VO.ResponseTemplateVO;
 import com.example.userdemo.entities.User;
 import com.example.userdemo.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 @Service
 @RequiredArgsConstructor
@@ -22,15 +28,24 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    @SneakyThrows
     public ResponseTemplateVO getUserWithDepartment(Long userId) {
         log.info("Inside getUserWithDepartment method of UserService");
         ResponseTemplateVO vo = new ResponseTemplateVO();
         User user = userRepository.findByUserId(userId);
 
-        Department department = restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/" + userId, Department.class);
-        vo.setUser(user);
-        vo.setDepartment(department);
+        try {
+            ResponseEntity<Department> response = restTemplate.getForEntity(new URI("http://API-GATEWAY/departments/" + userId),
+                    Department.class);
 
-        return vo;
+            vo.setUser(user);
+            vo.setDepartment(response.getBody());
+            return vo;
+        } catch (RestClientException exception) { // this rest client exception processing will be done in the exception handler and completely removed from here. It will return an appropriate status
+            if (((HttpServerErrorException.ServiceUnavailable) exception).getStatusCode().value() == 503) {
+                log.error("Called GET DepartmentById from UserService but the DepartmentService is down.");
+            }
+            throw exception;
+        }
     }
 }
